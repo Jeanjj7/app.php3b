e8ad1e482050dee0718dd6aad90f842dc95bcbfd
Aluno SENAI - AREIAS <jeanjunior69x@gmail.com>
2024-10-22 20:50:28 -0300

    22/10 php

---
+++ inserir.php
@@ -0,0 +1,21 @@
+<?php
+include "conexao.php";
+
+$nome = $_POST['primeiro_nome'];
+$email = $_POST['email_digitado'];
+
+// 1º Passo - Comando SQL
+$sql = "INSERT INTO tb_inscritos
+        (nome_inscrito, email_inscrito) VALUES
+        ('$nome', '$email')";
+// 2º Passo - Preparar a conexão
+$inserir = $pdo->prepare($sql);
+// 3º Passo - Tentar executar
+try{
+    $inserir->execute();
+    header("Location: agredecimento.html");
+
+}catch(PDOException $erro){
+    echo "Falha no inserir!".$erro->getMessage();
+}
+?>
